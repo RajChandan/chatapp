f7e3b34ef9e8d59064992304d0a66799b9354cdb
root <raj.chandan91@gmail.com>
2024-03-15 22:19:17 +0530

    channel routing

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,4 +9,29 @@ class ChatConsumer(WebsocketConsumer):
         self.room_name = None
         self.room_group_name = None
         self.room = None
-        
+    
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        print(self.room_name,' === room name')
+        print(self.scope,' === scope')
+        self.room_group_name = f'chat_{self.room_name}'
+        self.room = Room.objects.get(name=self.room_name)
+
+        self.accept()
+        print(self.channel_layer,' ==== channel layer')
+        print(self.channel_name,' === channnel name')
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name,self.channel_name)
+
+    
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name,self.channel_name)
+
+    
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        async_to_sync(self.channel_layer.group_send)(self.room_group_name,{'type':'chat_message','message':message})
+    
+    def chat_message(self,event):
+        self.send(text_data=json.dumps(event))
